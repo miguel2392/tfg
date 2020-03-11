a5ab0722956945329c30e86dad30ece69fa834a9
miguel2392 <miguel_23-92@hotmail.com>
2020-03-11 14:01:06 +0100

    intent presentacionActivity Id asignatura pasado a PresentacionActivity desde ProfesorActivity

--- app/src/main/java/com/example/myapplication/PresentacionActivity.java
+++ app/src/main/java/com/example/myapplication/PresentacionActivity.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PresentacionActivity extends AppCompatActivity {
+
+    private String idAsignatura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +23,33 @@ public class PresentacionActivity extends AppCompatActivity {
 
         recibirDatos();
 
+
+
+        Button empezarPresentacion = findViewById(R.id.buttonEmpezarPresentacion);
+        empezarPresentacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // método para crear documentos en la base de datos
+                createDbDocument();
+            }
+        });
+
     }
 
     private void recibirDatos(){
         Bundle idRecibido = getIntent().getExtras();
-        String idAsignatura = idRecibido.getString("Id asignatura");
+        idAsignatura = idRecibido.getString("Id asignatura");
         Log.d("!!!", idAsignatura);
+    }
+
+    private void createDbDocument(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> presentation = new HashMap<>();
+        presentation.put("nombre", "Los Angeles");
+
+
+        //db.collection("presentaciones").document(idAsignatura).set();
+
     }
 }
